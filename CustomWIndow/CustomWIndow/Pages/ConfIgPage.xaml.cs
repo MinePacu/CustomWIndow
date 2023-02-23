40c2963dfa0df7e1edab756118ef6269e6d30c40
MinePacu <subin11@hotmail.co.kr>
2023-02-23 23:56:30 +0900

    ConfigPage를 Mvvm 구조로 변경
    - ConfigPage.xaml, ConfigPage.xaml.cs -> Mvvm 구조 변경을 위한 코드 정리
    - ConfigPageViewModel.cs -> Mvvm 구조를 위한 ConfigPage의 뷰모델 파일 추가

--- CustomWIndow/CustomWIndow/Pages/ConfIgPage.xaml.cs
+++ CustomWIndow/CustomWIndow/Pages/ConfIgPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using Windows.ApplicationModel.Activation;
 using WinRT;
 using System;
+using CustomWIndow.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -25,42 +26,7 @@ namespace CustomWIndow.Pages
             this.InitializeComponent();
 
             if (SysFunction.IsAdmin())
-                RestartWithAdminbutton.IsEnabled = false;
-
-            AutoAdminToggle.IsOn = ConfIg.Instance.AutoAdmin;
-            IsTray = ConfIg.Instance.EtcConfIg.IsTray;
-        }
-
-        void AutoAdminToggle_Toggled(object sender, RoutedEventArgs e)
-        {
-            ConfIg.Instance.AutoAdmin = ((ToggleSwitch) sender).IsOn;
-        }
-
-        void RestartWithAdminbutton_Click(object sender, RoutedEventArgs e)
-        {
-            ConfIg.Save();
-
-            ProcessStartInfo psi = new(Environment.CurrentDirectory + "\\CustomWIndow.exe")
-            {
-                Verb = "Runas",
-                UseShellExecute = true,
-            };
-
-            try
-            {
-                Process.Start(psi);
-                Environment.Exit(0);
-            }
-
-            catch (Exception)
-            {
-
-            }
-        }
-
-        private void IsTrayToggle_Toggled(object sender, RoutedEventArgs e)
-        {
-            ConfIg.Instance.EtcConfIg.IsTray = ((ToggleSwitch)sender).IsOn;
+                ConfigPageViewmodel.IsRestartWithAdminEnable = false;
         }
     }
 }
---
+++ CustomWIndow/CustomWIndow/ViewModels/ConfigPageViewModel.cs
@@ -0,0 +1,80 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using CustomWIndow.UtIl;
+
+using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace CustomWIndow.ViewModels
+{
+    public class ConfigPageViewModel : ObservableObject
+    {
+        private bool isTray;
+
+        public bool IsTray
+        {
+            get => isTray;
+            set
+            {
+                SetProperty(ref isTray, value);
+                ConfIg.Instance.EtcConfIg.IsTray = value;
+            }
+        }
+
+        private bool isRestartWithAdminEnable;
+
+        public bool IsRestartWithAdminEnable
+        {
+            get => isRestartWithAdminEnable;
+            set
+            {
+                SetProperty(ref isRestartWithAdminEnable, value);
+            }
+        }
+
+        private bool isAutoAdmin;
+
+        public bool IsAutoAdmin
+        {
+            get => isAutoAdmin;
+            set
+            {
+                SetProperty(ref isAutoAdmin, value);
+                ConfIg.Instance.AutoAdmin = value;
+            }
+        }
+
+        public ICommand RestartAppWithAdminCommand { get; }
+
+        public ConfigPageViewModel()
+        {
+            IsAutoAdmin = ConfIg.Instance.AutoAdmin;
+            IsTray = ConfIg.Instance.EtcConfIg.IsTray;
+
+            RestartAppWithAdminCommand = new RelayCommand(RestartAppWithAdmin);
+        }
+
+        public void RestartAppWithAdmin()
+        {
+            ConfIg.Save();
+            ProcessStartInfo psi = new(Environment.CurrentDirectory + "\\CustomWIndow.exe")
+            {
+                Verb = "Runas",
+                UseShellExecute = true,
+            };
+
+            try
+            {
+                Process.Start(psi);
+                Environment.Exit(0);
+            }
+
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
